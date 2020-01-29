c17ed68a51fbd20d1caec7b479518be23b525eb7
CannedGrape <cannedgrape@outlook.com>
2020-01-29 15:01:46 +0800

    i686->x86_64

--- test/.libs/lt-amrnb-dec.c
+++ test/.libs/lt-amrnb-dec.c
@@ -159,7 +159,7 @@ volatile const char * MAGIC_EXE = "%%%MAGIC EXE variable%%%";
 const char * LIB_PATH_VARNAME = "PATH";
 const char * LIB_PATH_VALUE   = "D:\\HOME\\AMR_Codec\\opencore-amr-0.1.3\\amrnb\\.libs;";
 const char * EXE_PATH_VARNAME = "PATH";
-const char * EXE_PATH_VALUE   = "D:\\HOME\\AMR_Codec\\opencore-amr-0.1.3\\amrnb\\.libs;C:\\msys32\\usr\\local\\lib;C:\\msys32\\usr\\local\\bin;";
+const char * EXE_PATH_VALUE   = "D:\\HOME\\AMR_Codec\\opencore-amr-0.1.3\\amrnb\\.libs;C:\\msys64\\mingw64\\lib;C:\\msys64\\mingw64\\bin;";
 const char * TARGET_PROGRAM_NAME = "amrnb-dec.exe"; /* hopefully, no .exe */
 
 #define LTWRAPPER_OPTION_PREFIX         "--lt-"
@@ -1025,8 +1025,8 @@ void lt_dump_script (FILE* f)
   fputs ("\n", f);
   fputs ("  if test -f \"$progdir/$program\"; then\n", f);
   fputs ("    # Add the dll search path components to the executable PATH\n", f);
-  fputs ("    PATH=/d/HOME/AMR_Codec/opencore-amr-0.1.3/amrnb/.libs:/usr/local/lib:/usr/l", f);
-  fputs ("ocal/bin:$PATH\n", f);
+  fputs ("    PATH=/d/HOME/AMR_Codec/opencore-amr-0.1.3/amrnb/.libs:/mingw64/lib:/mingw64", f);
+  fputs ("/bin:$PATH\n", f);
   fputs ("\n", f);
   fputs ("    # Add our own library path to PATH\n", f);
   fputs ("    PATH=\"/d/HOME/AMR_Codec/opencore-amr-0.1.3/amrnb/.libs:$PATH\"\n", f);
